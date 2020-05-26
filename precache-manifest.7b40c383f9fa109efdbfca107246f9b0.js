5f7ff08058c1fb9423d09db39596ec4deb832202
Bob Shields <bobcatshields@gmail.com>
2020-05-26 11:46:32 -0400

    Updates

---
+++ precache-manifest.7b40c383f9fa109efdbfca107246f9b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f8b538cd018b988c31aac68605c9419",
+    "url": "/colour-boxes/index.html"
+  },
+  {
+    "revision": "5e545b2a59cdd0ebcd85",
+    "url": "/colour-boxes/static/css/main.82d873fc.chunk.css"
+  },
+  {
+    "revision": "b5b81fd9292161ca9d90",
+    "url": "/colour-boxes/static/js/2.4bb97315.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/colour-boxes/static/js/2.4bb97315.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e545b2a59cdd0ebcd85",
+    "url": "/colour-boxes/static/js/main.94787a21.chunk.js"
+  },
+  {
+    "revision": "e20f2a700f45575bfbd3",
+    "url": "/colour-boxes/static/js/runtime-main.d4a57551.js"
+  }
+]);
